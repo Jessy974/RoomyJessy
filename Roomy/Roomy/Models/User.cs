37373861e6f4228b8bf938eedefc66f20f8a849a
Jessy <jessy@laniel.me>
2018-07-16 17:08:55 +0200

    Roomy Jessy

---
+++ Roomy/Roomy/Models/User.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Roomy.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Le champ {0} est obligatoire")]
+        [Display(Name ="Nom")]
+        [StringLength(50, MinimumLength = 2,
+            ErrorMessage ="Le champ {0} doit contenir entre {2} et {1} caractères")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Prénom")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est obligatoire")]
+        [Display(Name = "Email")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+                           @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+                           @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",
+        ErrorMessage = "L'adresse mail n'est pas au bon format")]
+        public string Mail { get; set; }
+
+
+        [Required(ErrorMessage = "Le champ {0} est obligatoire")]
+        [Display(Name = "Date de naissance")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est obligatoire")]
+        [Display(Name = "Mot de passe")]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{6,}$", 
+            ErrorMessage = "mot de passe incorrect")]
+        public string  Password { get; set; }
+
+        
+        [Required(ErrorMessage = "Le champ {0} est obligatoire")]
+        [Display(Name = "Confirmez votre mot de passe ")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage ="Erreur sur la confirmation du mot de passe")]
+        public string ConfirmedPassword { get; set; }
+        
+    }
+
+}
